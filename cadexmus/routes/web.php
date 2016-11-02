1751138c6724ce8a8e511baab7e888ebb0908507
arnaud droxler <arnaud.droxler@gmail.com>
2016-11-02 13:33:21 +0100

    ajout du projet auth

--- cadexmus/routes/web.php
+++ cadexmus/routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
