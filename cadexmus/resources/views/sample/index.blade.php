315432f0f6ce1b731e0b240385be45ab5ba7971e
joaquim <joaquim.perez@he-arc.ch>
2016-11-16 19:43:29 +0100

    resolution de conflit

--- cadexmus/resources/views/sample/index.blade.php
+++ cadexmus/resources/views/sample/index.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<<<<<<< HEAD
-    <a href="{{ route('sample.create')}}">create</a>
-=======
 
 @foreach ($samples as $s)
     <p>{{ $s->id }} : {{ $s->nom }}</p>
@@ -13,5 +10,5 @@
 @endforeach
 <br>
 <a href="{{ route('sample.create')}}">upload sample</a>
->>>>>>> 6e44632add2d1a481344a830570ff8f76c0ebb60
+
 @endsection
