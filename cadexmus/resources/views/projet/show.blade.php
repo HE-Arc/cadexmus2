ae016c5d3d9afff6a1ff8a4c0c083958b769a539
joaquim <joaquim.perez@he-arc.ch>
2016-11-21 20:11:38 +0100

    affichage de l'objet repr plus sympa

--- cadexmus/resources/views/projet/show.blade.php
+++ cadexmus/resources/views/projet/show.blade.php
@@ -3,5 +3,16 @@
 @section('content')
     <h1>Projet {{ $projet->nom }}</h1>
     <h2>Version {{$version->numero}}</h2>
-    <code>{{json_encode($version->repr, JSON_UNESCAPED_SLASHES)}}</code>
+
+    <p>tempo : {{$version->repr["tempo"]}}</p>
+    <ul>
+        @foreach ($version->repr["tracks"] as $track)
+            <li>{{ $track["sample"]["name"] }}</li>
+            <ul>
+                @foreach($track["notes"] as $note)
+                    <li>pos:{{$note["pos"]}}, len:{{$note["len"]}}</li>
+                @endforeach
+            </ul>
+        @endforeach
+    </ul>
 @endsection
