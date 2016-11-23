6f336a423f8f60b3a21b7ea2966a08b4ab0e9dff
joaquim <joaquim.perez@he-arc.ch>
2016-11-23 12:26:20 +0100

    preparation pour modification du projet

--- cadexmus/resources/views/projet/show.blade.php
+++ cadexmus/resources/views/projet/show.blade.php
@@ -5,16 +5,46 @@
     <h1>Projet {{ $projet->nom }}</h1>
     <h2>Version {{$version->numero}}</h2>
 
-    <p>tempo : {{$version->repr["tempo"]}}</p>
-    <ul>
-        @foreach ($version->repr["tracks"] as $track)
-            <li>{{ $track["sample"]["name"] }}
+    <p>tempo : <input type="number" value="{{$version->repr["tempo"]}}"></p>
+    <ul id="tracks">
+    @foreach ($version->repr["tracks"] as $track)
+        <li class="track">
+            {{ $track["sample"]["name"] }}
+            <audio controls src="../uploads/{{$track["sample"]["url"]}}" style="vertical-align: middle"></audio>
+            <button>remove</button>
             <ul>
                 @foreach($track["notes"] as $note)
-                    <li>pos:{{$note["pos"]}}, len:{{$note["len"]}}</li>
+                <li class="note">
+                    pos:<input type="number" value="{{$note["pos"]}}">,
+                    len:<input type="number" value="{{$note["len"]}}">
+                    <button>remove</button>
+                </li>
                 @endforeach
+                <li><button>add note</button></li>
             </ul>
-            </li>
-        @endforeach
+        </li>
+    @endforeach
+        <li>
+            <p>
+                Sample name :
+                <input type="text" name="samplename">
+                URL :
+                <input type="text" name="sampleurl">
+                <button onclick="addTrack()">add track</button>
+            </p>
+        </li>
     </ul>
+
+    <button onclick="save()">Save</button>
+
+
+    <script>
+        function addTrack(btn){
+
+        }
+
+        function save() {
+            alert("saving");
+        }
+    </script>
 @endsection
