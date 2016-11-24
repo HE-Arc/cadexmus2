f8fc15de9c73a69f83e89248127c5f1c5492f2ac
joaquim <joaquim.perez@he-arc.ch>
2016-11-24 22:43:16 +0100

    pas si inutile... je suis débile

--- cadexmus/resources/views/sample/index.blade.php
+++ cadexmus/resources/views/sample/index.blade.php
@@ -1,6 +1,9 @@
-@extends('layouts.app')
-
-
-@section('content')
-    @include('sample.choose')
-@endsection
+<ul>
+@foreach ($samples as $s)
+    <li class="sample" sampleName="{{$s->nom}}" sampleUrl="{{$s->url}}" style="cursor: pointer;">
+        <p>{{ $s->id }} : {{ $s->nom }}</p>
+        <audio controls src="{{asset("uploads")}}/{{$s->url}}"></audio>
+    </li>
+@endforeach
+</ul>
+@include('sample.create')
