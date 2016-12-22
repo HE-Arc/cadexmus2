0e3594f1083a473f0a9c6d1316d824aa6b63c1c1
Bastien Burri <bastien.burri@he-arc.ch>
2016-12-22 02:57:08 +0100

    Rendre le nom d'utilisateur unique

--- cadexmus/app/Http/Controllers/Auth/RegisterController.php
+++ cadexmus/app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
+            'name' => 'required|max:255|unique:users',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
             'picture' => 'mimetypes:image/gif,image/jpeg,image/png'
