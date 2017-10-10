f682348ddfcfd1f903958132dd6da050ce5455c5
joaquim <jivkim@gmail.com>
2017-10-11 01:43:30 +0200

    login redirige sur /

--- cadexmus/app/Http/Controllers/Auth/LoginController.php
+++ cadexmus/app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/projet';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
